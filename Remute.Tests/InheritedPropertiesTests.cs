bdba43c2596ce77d05a5a87077d71e126dc467d5
Alexey Babik <alexey.babik@gmail.com>
2020-02-25 15:51:12 -0800

    Fix formating

--- Remute.Tests/InheritedPropertiesTests.cs
+++ Remute.Tests/InheritedPropertiesTests.cs
@@ -7,38 +7,38 @@ namespace Remutable.Tests
     {
         [TestMethod]
         public void TestBaseInheritedProperty1_Success()
-		{
-			var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
-			var actual = Remute.Default.With(instance, x => x.Prop1, "update");
-			Assert.AreEqual("update", actual.Prop1);
-			Assert.AreEqual(instance.Prop2, actual.Prop2);
-			Assert.AreEqual(instance.Prop3, actual.Prop3);
-			Assert.AreEqual(instance.Prop4, actual.Prop4);
-		}
+        {
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = Remute.Default.With(instance, x => x.Prop1, "update");
+            Assert.AreEqual("update", actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+            Assert.AreEqual(instance.Prop4, actual.Prop4);
+        }
 
-		[TestMethod]
+        [TestMethod]
         public void TestBaseInheritedProperty2_Success()
-		{
-			var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
-			var actual = Remute.Default.With(instance, x => x.Prop4, "update");
-			Assert.AreEqual("update", actual.Prop4);
-			Assert.AreEqual(instance.Prop1, actual.Prop1);
-			Assert.AreEqual(instance.Prop2, actual.Prop2);
-			Assert.AreEqual(instance.Prop3, actual.Prop3);
-		}
+        {
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = Remute.Default.With(instance, x => x.Prop4, "update");
+            Assert.AreEqual("update", actual.Prop4);
+            Assert.AreEqual(instance.Prop1, actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+        }
 
-		[TestMethod]
+        [TestMethod]
         public void TestConfig_Success()
-		{
-			var config = new ActivationConfiguration()
-				.Configure<InheritedType4>(x => new InheritedType4(x.Prop1, x.Prop2, x.Prop3, x.Prop4));
-			var remute = new Remute(config);
-			var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
-			var actual = remute.With(instance, x => x.Prop1, "update");
-			Assert.AreEqual("update", actual.Prop1);
-			Assert.AreEqual(instance.Prop2, actual.Prop2);
-			Assert.AreEqual(instance.Prop3, actual.Prop3);
-			Assert.AreEqual(instance.Prop4, actual.Prop4);
-		}
+        {
+            var config = new ActivationConfiguration()
+                .Configure<InheritedType4>(x => new InheritedType4(x.Prop1, x.Prop2, x.Prop3, x.Prop4));
+            var remute = new Remute(config);
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = remute.With(instance, x => x.Prop1, "update");
+            Assert.AreEqual("update", actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+            Assert.AreEqual(instance.Prop4, actual.Prop4);
+        }
     }
 }
