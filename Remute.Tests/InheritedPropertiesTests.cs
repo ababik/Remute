8f4a6911b2337390e6d3b433a9273123eeeb1833
Alexey Babik <alexey.babik@gmail.com>
2020-02-25 15:56:05 -0800

    Inherited properties (#17)
    * Fix inherited properties
    
    * Inherited properties tests
    
    * Fix formating

---
+++ Remute.Tests/InheritedPropertiesTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Remutable.Tests
+{
+    [TestClass]
+    public class InheritedPropertiesTests
+    {
+        [TestMethod]
+        public void TestBaseInheritedProperty1_Success()
+        {
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = Remute.Default.With(instance, x => x.Prop1, "update");
+            Assert.AreEqual("update", actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+            Assert.AreEqual(instance.Prop4, actual.Prop4);
+        }
+
+        [TestMethod]
+        public void TestBaseInheritedProperty2_Success()
+        {
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = Remute.Default.With(instance, x => x.Prop4, "update");
+            Assert.AreEqual("update", actual.Prop4);
+            Assert.AreEqual(instance.Prop1, actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+        }
+
+        [TestMethod]
+        public void TestConfig_Success()
+        {
+            var config = new ActivationConfiguration()
+                .Configure<InheritedType4>(x => new InheritedType4(x.Prop1, x.Prop2, x.Prop3, x.Prop4));
+            var remute = new Remute(config);
+            var instance = new InheritedType4("prop1", "prop2", "prop3", "prop4");
+            var actual = remute.With(instance, x => x.Prop1, "update");
+            Assert.AreEqual("update", actual.Prop1);
+            Assert.AreEqual(instance.Prop2, actual.Prop2);
+            Assert.AreEqual(instance.Prop3, actual.Prop3);
+            Assert.AreEqual(instance.Prop4, actual.Prop4);
+        }
+    }
+}
--- Remute/ActivationSetting.cs
+++ Remute/ActivationSetting.cs
@@ -13,20 +13,21 @@ namespace Remutable
 
         public ActivationSetting(ConstructorInfo constructor, Dictionary<ParameterInfo, PropertyInfo> parameters)
         {
-            parameters.ToList().ForEach(x => Validate(constructor, x.Key, x.Value));
+            var properties = Remute.GetUsableProperties(constructor.DeclaringType);
+            parameters.ToList().ForEach(x => Validate(constructor, x.Key, x.Value, properties));
 
             Constructor = constructor;
             Parameters = parameters;
         }
 
-        private static void Validate(ConstructorInfo constructor, ParameterInfo parameter, PropertyInfo property)
+        private static void Validate(ConstructorInfo constructor, ParameterInfo parameter, PropertyInfo property, PropertyInfo[] properties)
         {
             if (parameter.Member != constructor)
             {
                 throw new Exception($"Invalid parameter '{parameter.Name}'. Parameter must be a member of '{constructor.DeclaringType}' constructor.");
             }
 
-            if (property.DeclaringType != constructor.DeclaringType)
+            if (properties.SingleOrDefault(x => Remute.SameMembers(x, property)) is null)
             {
                 throw new Exception($"Invalid property '{property.Name}'. Must be a member of '{constructor.DeclaringType}'.");
             }
